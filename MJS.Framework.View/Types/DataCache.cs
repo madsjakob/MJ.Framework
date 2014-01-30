4317467308eb242b978a784deab74f19b0ec5ec3
Mads Jakob Steffansen <mads.steffansen@gmail.com>
2014-01-30 21:59:00 +0100

    Add DataCache

---
+++ MJS.Framework.View/Types/DataCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJS.Framework.View.Types
+{
+    public class DataCache : Dictionary<Guid, ViewObject>
+    {
+        // Private constructor to ensure singleton
+        private DataCache()
+        {
+        }
+        // A cache for entities from the database
+        // Keeps track of data 
+        private static DataCache _cache;
+        public static DataCache Cache
+        {
+            get
+            {
+                if (_cache == null)
+                {
+                    _cache = new DataCache();
+                }
+                return _cache;
+            }
+        }
+
+        public ViewObject LoadEntity<T>(Guid id)
+        {
+            if (!_cache.ContainsKey(id) || _cache[id].Dirty)
+            {
+                // Load entity
+            }
+            return _cache[id];
+        }
+
+        public void EditEntity<T>(Guid id)
+        { 
+        }
+    }
+}
--- MJS.Framework.View/Types/ViewObject.cs
+++ MJS.Framework.View/Types/ViewObject.cs
@@ -9,7 +9,33 @@ namespace MJS.Framework.View.Types
 {
     public class ViewObject
     {
+        // Must be able to load both indexfields and the blob
+        // Must be able to save both indexfields and the blob
+
+        // We need an ID to save to
+        // Properties must be marked with an xpath and if necessary an indexfield
+
+        private Guid _id;
+        private Type _entityType;
+        private byte[] _blob;
         private XmlDocument _data;
+
+        public bool Dirty
+        {
+            get { return false; }
+        }
+        public DateTime LoadTime
+        {
+            get { return DateTime.Now; }
+        }
+        public bool EditMode
+        {
+            get { return false; }
+        }
+        public bool Locked
+        {
+            get { return false; }
+        }
 
         public bool Load()
         {

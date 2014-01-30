2f95cf34237411c562a0bd6ba459af930e79c439
Mads Jakob Steffansen <mads.steffansen@gmail.com>
2014-01-30 20:13:45 +0100

    Add ViewObject

---
+++ MJS.Framework.View/Types/ViewObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MJS.Framework.View.Types
+{
+    public class ViewObject
+    {
+        private XmlDocument _data;
+
+        public bool Load()
+        {
+            return false;
+        }
+
+        public void Save()
+        {
+        }
+    }
+}

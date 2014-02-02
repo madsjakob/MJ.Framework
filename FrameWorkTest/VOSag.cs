70b5d388bb5a03fb720648f8bfc1157a8dbe77b6
Mads Jakob Steffansen <mads.steffansen@gmail.com>
2014-02-02 20:15:08 +0100

    Introduced DD=DataDescription (object) 2

---
+++ FrameWorkTest/VOSag.cs
@@ -0,0 +1,17 @@
+﻿using MJS.Framework.View.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameWorkTest
+{
+    [DataObject("Sag", "SagID", "Opdateret", BlobField = "Blobrec")]
+    public class VOSag : ViewObject<DDSag>
+    {
+        public VOSag(Guid id) : base(id)
+        {
+        }
+    }
+}
